8f775496dae6baded42fb9e31eeb905312fd31da
WumaCoder <1829913225@qq.com>
2021-09-28 17:42:47 +0800

    v1.0.6

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "rollup";
 import typescript from "rollup-plugin-typescript2";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { uglify } from "rollup-plugin-uglify";
 
 export default defineConfig({
   input: "src/index.ts",
@@ -13,6 +14,17 @@ export default defineConfig({
       file: "libs/ctxexp-parser.cjs.js",
       format: "cjs",
       exports: "auto",
+    },
+    {
+      file: "libs/ctxexp-parser.esm.min.js",
+      format: "esm",
+      plugins: [uglify()],
+    },
+    {
+      file: "libs/ctxexp-parser.cjs.min.js",
+      format: "cjs",
+      exports: "auto",
+      plugins: [uglify()],
     },
   ],
   plugins: [
